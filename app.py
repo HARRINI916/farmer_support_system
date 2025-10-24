a90893cc7d265f81aa3aa3389e0f97bd114e09a7
HARRINI916 <harrinids916@gmail.com>
2025-10-24 10:40:56 +0530

    Fixed AttributeError and updated market_price module

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import streamlit as st
 from modules import market_price, crop_recommendation, query_system
-from modules import market_price
-
-market_price.show_market_price()
-
-
 
 st.title("🚜 விவசாயி ஆதரவு அமைப்பு")
 
-# Sidebar for options
 option = st.sidebar.selectbox(
     "தேர்வு செய்யவும்:",
     ["சந்தை விலை", "பயிர் பரிந்துரை", "விவசாயி கேள்வி"]
@@ -17,7 +11,6 @@ option = st.sidebar.selectbox(
 if option == "சந்தை விலை":
     market_price.show_market_price()
 elif option == "பயிர் பரிந்துரை":
-    crop_suggestion.suggest_crops()
-
+    crop_recommendation.suggest_crops()
 elif option == "விவசாயி கேள்வி":
     query_system.farmer_query()
